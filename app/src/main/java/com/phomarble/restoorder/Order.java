9bba5c4bcc5cf38b5dac46e6d9b60684b988bbdc
DanielTran3 <dtran3@ualberta.ca>
2017-05-02 17:25:58 -0600

    Changed icon, new activity for orders

--- app/src/main/java/com/phomarble/restoorder/Order.java
+++ app/src/main/java/com/phomarble/restoorder/Order.java
@@ -2,4 +2,10 @@ package com.phomarble.restoorder;
 
 public class Order {
     private int quantity;
+    private MenuItem foodItem;
+
+    public Order(int quantity, MenuItem item) {
+        this.quantity = quantity;
+        this.foodItem = item;
+    }
 }
